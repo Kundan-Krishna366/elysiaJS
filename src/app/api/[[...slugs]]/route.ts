1c84366a66fc529875e478eab710d15398f4671c
kundan-krishna366 <kundan.krishna366@gmail.com>
2025-12-13 16:17:41 +0530

    Add authentication middleware and real-time messaging support

--- src/app/api/[[...slugs]]/route.ts
+++ src/app/api/[[...slugs]]/route.ts
@@ -1,6 +1,9 @@
 import { Elysia, t } from 'elysia'
 import { nanoid } from 'nanoid'
 import { redis } from '@/lib/redis'
+import { authMiddleware } from './auth'
+import { z } from 'zod'
+import { send } from 'process'
 const rooms = new Elysia({prefix:"/room"}).post("/", async()=>{
     const roomId = nanoid()
 
@@ -13,7 +16,29 @@ const rooms = new Elysia({prefix:"/room"}).post("/", async()=>{
 
     return {roomId}
 })
-const app = new Elysia({ prefix: '/api' }).use(rooms)
+
+const messages = new Elysia({prefix:"/messages"})
+.use(authMiddleware).post("/",async({body,auth})=>{
+    const {sender,text} = body
+    const {roomId} = auth
+    const roomExists = await redis.exists(`meta:${roomId}`)
+    if(!roomExists){
+        throw new Error("Room does not exist")
+    }
+},
+    {
+    query: z.object({
+        roomId: z.string()
+    }),
+    body: z.object({
+        sender: z.string().min(1).max(30),
+        text: z.string().min(1).max(1000)
+    })
+    })
+
+
+
+const app = new Elysia({ prefix: '/api' }).use(rooms).use(messages)
 
 export const GET = app.fetch 
 export const POST = app.fetch 
